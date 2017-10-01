68258fe5c4d2f635a9b3277964753f499c18c80a
Ben Neeman <bentechnyc@gmail.com>
2017-10-01 15:36:18 +0000

    plop

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,27 +3,11 @@ class TodoController<ApplicationController
     end
     def show 
         todo_id = params[:id].to_i
+        @todo=Todo.find_by_id(todo_id)
      
-       
-        if todo_id == 1
-            @pomo_est = 1
-            @todo_description = "do laundry"
         
-        elsif todo_id == 2
-            @pomo_est = 1
-            @todo_description = "pet cat"
-        
-        elsif todo_id == 3
-            @pomo_est = 3
-            @todo_description ="do essay"
-        
-        elsif todo_id == 4
-            @pomo_est = 4
-            @todo_description = "cry"
-        end
-            
-        #when user clicks item 1, make description be wash car 
     end
     
     
-end
+
+end 
