8a7f440f90d3030022f0e30c6120daca0e2da069
Ben Neeman <bentechnyc@gmail.com>
2017-10-01 18:23:38 +0000

    puuup

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -20,4 +20,12 @@ class TodoController<ApplicationController
     def edit
         @todo = Todo.find_by_id(params['id'])
     end
+    
+    def update 
+        t = Todo.find_by_id(params['id'])
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
 end 
